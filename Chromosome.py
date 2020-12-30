9df8555541e3902130f3acbe380b35dfbebbbb00
zelimirmaletic <mzeljo417@outlook.com>
2020-12-30 23:09:14 +0100

    Updated chromosome for 2D case

--- Chromosome.py
+++ Chromosome.py
@@ -58,6 +58,7 @@ class Chromosome:
 
     #Methond for recombining two chromosomes
     def recombine(self, otherChromosome):
+        """X-coordinate"""
         #Choose a point on which we break DNA and interchange right parts
         randomNumber = random.uniform(0,1)
         #Transform random number to a segment [1 - len(DNA)]
@@ -72,9 +73,27 @@ class Chromosome:
         #Regenerate coordinates from new DNAs
         self.setX(bin.decodeBinary(self.getDNASequenceX(), fun.LOWER_X, fun.UPPER_X))
         otherChromosome.setX(bin.decodeBinary(otherChromosome.getDNASequenceX(), fun.LOWER_X, fun.UPPER_X))
-    
+        """Y-coordinate"""
+        #Choose a point on which we break DNA and interchange right parts
+        randomNumber = random.uniform(0,1)
+        #Transform random number to a segment [1 - len(DNA)]
+        disectionPoint = m.floor(bin.transformToInterval(randomNumber,0,1,1,len(self.yDNA)))
+        #Now disect both DNAs and get sequence right of disection point
+        sequence1 = self.yDNA[disectionPoint:]
+        sequence2 = (otherChromosome. getDNASequenceY())[disectionPoint:]
+        #Interchange the parts of DNA right from disection point
+        self.setDnaSequenceY(self.yDNA.replace(self.yDNA[disectionPoint:], sequence2))
+        dna = otherChromosome.getDNASequenceY()
+        otherChromosome.setDnaSequenceY(dna.replace(dna[disectionPoint:],sequence1))
+        #Regenerate coordinates from new DNAs
+        self.setY(bin.decodeBinary(self.getDNASequenceY(), fun.LOWER_Y, fun.UPPER_Y))
+        otherChromosome.setY(bin.decodeBinary(otherChromosome.getDNASequenceY(), fun.LOWER_Y, fun.UPPER_Y))
+
+
+
     #Method for mutating a chromosome
     def mutate(self):
+        """X-coordinate"""
         #Generate random number
         randomNumber = random.uniform(0,1)
         #Transform to interval [0, len(DNA)]
@@ -88,6 +107,20 @@ class Chromosome:
         self.setDnaSequenceX("".join(dnaList))
         #Regenerate coordinates from new DNAs
         self.setX(bin.decodeBinary(self.getDNASequenceX(), fun.LOWER_X, fun.UPPER_X))
+        """Y-coordinate"""
+        #Generate random number
+        randomNumber = random.uniform(0,1)
+        #Transform to interval [0, len(DNA)]
+        index = m.floor(bin.transformToInterval(randomNumber,0,1,0,len(self.yDNA)))
+        #Invert bit in DNA sequence
+        dnaList = list(self.yDNA)
+        if(dnaList[index]=='1'):
+            dnaList[index]='0'
+        else:
+            dnaList[index]='1'
+        self.setDnaSequenceY("".join(dnaList))
+        #Regenerate coordinates from new DNAs
+        self.setY(bin.decodeBinary(self.getDNASequenceY(), fun.LOWER_Y, fun.UPPER_Y))
 
     def printChromosome(self):
         print("Chromosome ")
--- function.py
+++ function.py
@@ -9,6 +9,7 @@ UPPER_Y = 4.00
 
 def mathFunction(x,y):
     #y =x*x + 10.0*m.cos(x-5.00)
+    #Minimum of this function is at x=1.544 and it is y=-7.126
 
     a = -(x*x+pow((y+1),2))
     b = -(x*x+y*y)
@@ -16,5 +17,5 @@ def mathFunction(x,y):
     z = 3*pow((1-x),2)*pow(m.e,a) - 7*(x/5 - pow(x,3) - pow(y,5))*pow(m.e,b)-(1/3)*pow(m.e,c)
     return z
 
-#Minimum of this function is at x=1.544 and it is y=-7.126
+
 
